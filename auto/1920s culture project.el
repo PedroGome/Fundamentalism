6ce7e9d1ad0784888d79e2661fec108ddadc9ed5
ZenTauro <pedrogomezmartin1999@gmail.com>
2017-05-16 20:28:55 -0400

    initial

---
+++ auto/1920s culture project.el
@@ -0,0 +1,28 @@
+(TeX-add-style-hook
+ "1920s culture project"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt" "letterpaper")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8") ("babel" "american") ("fontenc" "T1") ("biblatex" "backend=biber" "style=mla-new")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "inputenc"
+    "babel"
+    "fontenc"
+    "helvet"
+    "enumitem"
+    "ifpdf"
+    "mla"
+    "csquotes"
+    "biblatex")
+   (LaTeX-add-bibliographies
+    "bibliography"))
+ :latex)
+
